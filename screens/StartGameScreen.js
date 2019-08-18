a8a21fea44a3e85c7774ae146d12da8095197519
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-08-18 12:45:05 +0400

    add: color variable file

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,6 +2,7 @@ import React from "react";
 import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
 import Card from "../components/Card";
+import Colors from "../constants/colors";
 
 const StartGameScreen = props => {
   return (
@@ -11,8 +12,12 @@ const StartGameScreen = props => {
         <Text>Select a Number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
-          <Button title="Confirm" onPress={() => {}} />
-          <Button title="Reset" onPress={() => {}} />
+          <View style={styles.button}>
+            <Button title="Confirm" onPress={() => {}} color={Colors.primary} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Reset" onPress={() => {}} color={Colors.secondary} />
+          </View>
         </View>
       </Card>
     </View>
@@ -39,6 +44,9 @@ const styles = StyleSheet.create({
     width: "100%",
     justifyContent: "space-between",
     paddingHorizontal: 15
+  },
+  button: {
+    width: 100
   }
 });
 export default StartGameScreen;
